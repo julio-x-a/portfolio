69928874522b9e0ad7a6518dd047a0179488da05
Julio <jrab997@gmail.com>
2022-02-17 12:01:49 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const d = document;
+
+d.addEventListener('DOMContentLoaded', (e) => {
+  const typed = new Typed('#typed-title', {
+    strings: ['Julio Acevedo', 'Portfolio.'],
+    typeSpeed: 40
+  });
+});
